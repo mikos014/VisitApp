23892c2aa472f2d6eed126350f8bc3f65718f24b
slenderm97 <mikigreg97@gmail.com>
2020-01-26 23:07:33 +0100

    Working at registration...

--- app/src/main/java/wat/edu/pl/visitapp/view/welcome/RegisterActivity.java
+++ app/src/main/java/wat/edu/pl/visitapp/view/welcome/RegisterActivity.java
@@ -7,6 +7,7 @@ import wat.edu.pl.visitapp.R;
 
 public class RegisterActivity extends AppCompatActivity
 {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
