f19e26e716ae8ec01c68bb7d437eab9b677e6f7c
slenderm97 <mikigreg97@gmail.com>
2020-01-29 20:24:42 +0100

    Sidebar, drawer and toolbar

--- app/src/main/java/wat/edu/pl/visitapp/view/authenticated/MainActivity.java
+++ app/src/main/java/wat/edu/pl/visitapp/view/authenticated/MainActivity.java
@@ -1,14 +1,21 @@
 package wat.edu.pl.visitapp.view.authenticated;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import wat.edu.pl.visitapp.R;
 import wat.edu.pl.visitapp.database.entity.User;
 
 public class MainActivity extends AppCompatActivity
 {
+    private DrawerLayout drawer;
     private User user;
 
     @Override
@@ -23,5 +30,26 @@ public class MainActivity extends AppCompatActivity
         {
             user = (User) intent.getSerializableExtra("user");
         }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_sideBar_open, R.string.navigation_sideBar_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+            super.onBackPressed();
     }
 }
