88b3c200d8ad47203741731c1fbc315fd3f81507
slenderm97 <mikigreg97@gmail.com>
2020-03-12 16:44:18 +0100

    Some corrects, I hope last.

---
+++ app/src/main/java/wat/edu/pl/visitapp/database/entity/UserCreds.java
@@ -0,0 +1,98 @@
+package wat.edu.pl.visitapp.database.entity;
+
+public class UserCreds
+{
+    private String oldEmail;
+    private String email;
+    private String oldPassword;
+    private String password;
+    private String name;
+    private String surname;
+    private String dateOfBirth;
+    private String phoneNumber;
+    private int sex;
+
+    public UserCreds(String oldEmail, String email, String oldPassword, String password, String name, String surname, String dateOfBirth, String phoneNumber, int sex) {
+        this.oldEmail = oldEmail;
+        this.email = email;
+        this.oldPassword = oldPassword;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.sex = sex;
+    }
+
+    public String getOldEmail() {
+        return oldEmail;
+    }
+
+    public void setOldEmail(String oldEmail) {
+        this.oldEmail = oldEmail;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
